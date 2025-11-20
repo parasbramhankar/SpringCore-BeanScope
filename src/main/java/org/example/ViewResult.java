8b923dbda02dab87f53bc0d563ea9af539e2318c
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-20 19:47:26 +0530

    Update

---
+++ src/main/java/org/example/ViewResult.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ViewResult {
+
+    Result result;
+
+    void setStudent(Result result){
+        this.result=result;
+    }
+
+    void displayResult(){
+        int[]arr= result.marks();
+        System.out.println("Marks: "+ Arrays.toString(arr));
+
+        float avg= (float) Arrays.stream(arr).sum() /Arrays.stream(arr).count();
+
+        System.out.println("Average Marks: "+avg);
+    }
+
+}
