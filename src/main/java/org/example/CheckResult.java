218f101640ed612a0b59abe64d2fe7d7cef1a07c
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-20 19:47:14 +0530

    Update

--- src/main/java/org/example/CheckResult.java
+++ src/main/java/org/example/CheckResult.java
@@ -1,16 +1,20 @@
 package org.example;
 
-public class CheckResult {
+import java.util.Scanner;
 
-    Student student;
+public class CheckResult implements Result {
 
-    CheckResult(Student student){
-        this.student=student;
+
+    @Override
+    public int[] marks(){
+        Scanner scanner=new Scanner(System.in);
+        int []arr=new int[3];
+        System.out.println("Enter marks of the student: ");
+
+        for(int i=0;i<3;i++){
+            arr[i]=scanner.nextInt();
+        }
+        return arr;
     }
-
-    void displayResult(){
-        int[]arr= student.marks();
-
-    }
-
 }
+
