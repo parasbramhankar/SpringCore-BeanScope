881010be5dff919e39ada685ce4c7d927897fec0
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-21 12:05:15 +0530

    Final Update

--- src/main/java/org/example/ViewResult.java
+++ src/main/java/org/example/ViewResult.java
@@ -6,12 +6,13 @@ public class ViewResult {
 
     Result result;
 
-    void setResult(Result result){
-        this.result=result;
+    public void setResult(Result result) {
+        this.result = result;
     }
 
     void displayResult(){
         int[]arr= result.marks();
+
         System.out.println("Marks: "+ Arrays.toString(arr));
 
         float avg= (float) Arrays.stream(arr).sum() /Arrays.stream(arr).count();
