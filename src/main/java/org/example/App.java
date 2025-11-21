946ec6452122dc4d77ed519f1d71a03804c87a69
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-21 12:33:03 +0530

    Final Update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,7 +13,14 @@ public class App
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("Bean.xml");
         ViewStudent student = context.getBean(ViewStudent.class);
+        ViewStudent student1=context.getBean(ViewStudent.class);
+
         student.displayStudent();
+
+        System.out.println(student);
+        System.out.println(student1);
+
+        System.out.println(student1==student);
 
         ViewResult result=context.getBean(ViewResult.class);
         result.displayResult();
